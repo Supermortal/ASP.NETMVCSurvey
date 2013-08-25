98e831ddc3c622296ec59fc8974ff08913d41634
Chris Pink <christopherbyronpink@gmail.com>
2013-08-25 00:23:44 -0600

    Initial Commit

---
+++ MVCSurvey.Web/Scripts/xinha/init.js
@@ -0,0 +1,4 @@
+﻿_editor_url = "http://localhost:21734/Scripts/xinha/";   // (preferably absolute) URL (including trailing slash) where Xinha is installed
+_editor_lang = "en";       // And the language we need to use in the editor.
+//_editor_skin = "silva";    // If you want use a skin, add the name (of the folder) here
+//_editor_icons = "classic"; // If you want to use a different iconset, add the name (of the folder, under the `iconsets` folder) here
