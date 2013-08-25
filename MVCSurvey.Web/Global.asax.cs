4dc6231a644e884c43031879123d9f170cf0e154
Chris Pink <christopherbyronpink@gmail.com>
2013-08-24 23:56:47 -0600

    Initial Commit

---
+++ MVCSurvey.Web/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using MVCSurvey.Infrastructure.Concrete;
+using MVCSurvey.Web.App_Start;
+
+namespace MVCSurvey.Web
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+
+            log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
