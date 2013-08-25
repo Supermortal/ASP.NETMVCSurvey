8182b9c89ce4daf48d5dd1fd1e9b798e14d779fd
Chris Pink <christopherbyronpink@gmail.com>
2013-08-24 23:25:21 -0600

    Initial Commit

---
+++ MVCSurvey.Infrastructure/Abstract/IMenuOptionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NamespaceMedia.Infrastructure.Models.Shared;
+
+namespace NamespaceMedia.Infrastructure.Abstract
+{
+    public interface IMenuOptionRepository
+    {
+        IQueryable<MenuOption> GetAll();
+        IQueryable<MenuOption> GetTopLevel(string application);
+        MenuOption Find(long id);
+        void Save(MenuOption option);
+        void Delete(MenuOption option);
+        void Delete(long id);
+        void Update(MenuOption option);
+        IQueryable<MenuOption> SearchByApplication(string application);
+        IQueryable<MenuOption> GetChildren(string value);
+    }
+}
