980ca63da6a8266d935ce8396ea8689b15d495fc
Chris Pink <christopherbyronpink@gmail.com>
2013-08-25 00:06:48 -0600

    Initial Commit

---
+++ MVCSurvey.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCSurvey.Web.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
